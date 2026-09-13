f785506e411789e4dd91a3ac5a91c0ead4784c31
shahin6452 <shahin.shohag6452@gmail.com>
2026-09-13 15:22:58 +0600

    promise and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
+import { Suspense, useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Technologies from "./components/Technologies";
+import YourStack from "./components/YourStack";
+import type { TechnologyType } from "./Type";
 
-import './App.css'
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-
-const dataPromise = async() => {
-  const res = await fetch('/data.json')
-  const data = await res.json()
-  return data.countries
-}
+const dataPromise = async () => {
+  const res = await fetch("/technologies.json");
+  const data = await res.json();
+  return data;
+};
 
 function App() {
-  
+  const [selectedStack, setSelectedStack] = useState<TechnologyType[]>([]);
+
+  const handleAddToStack = (technology: TechnologyType) => {
+    const alreadyAdded = selectedStack.find(
+      (item) => item.id === technology.id
+    );
+
+    if (alreadyAdded) {
+      alert("This technology is already added!");
+      return;
+    }
+
+    setSelectedStack([...selectedStack, technology]);
+  };
+
+  const handleRemove = (id: string) => {
+    const remainingStack = selectedStack.filter(
+      (item) => item.id !== id
+    );
+
+    setSelectedStack(remainingStack);
+  };
+
+  const handleRemoveAll = () => {
+    setSelectedStack([]);
+  };
 
   return (
     <>
-    <div>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      
-    </div>
+      <Navbar />
+      <Hero />
+
+      <main className="bg-gray-50">
+        <div className="mx-auto max-w-7xl px-5 py-10">
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_300px]">
+
+            <Suspense fallback={<p>Loading...</p>}>
+              <Technologies
+                dataPromise={dataPromise()}
+                handleAddToStack={handleAddToStack}
+                selectedStack={selectedStack}
+              />
+            </Suspense>
+
+            <YourStack
+              selectedStack={selectedStack}
+              handleRemove={handleRemove}
+              handleRemoveAll={handleRemoveAll}
+            />
+
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
